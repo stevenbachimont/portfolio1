4b4c35625e10f88476be348e8a302f8a198f7799
stevenbachimont <142581911+stevenbachimont@users.noreply.github.com>
2023-09-26 18:40:55 +0200

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = 'flex';
+}
+
+window.addEventListener('click', function(event) {
+  var modals = document.getElementsByClassName('modal');
+  for (var i = 0; i < modals.length; i++) {
+    if (event.target == modals[i]) {
+      modals[i].style.display = 'none';
+    }
+  }
+});
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
+
+document.getElementById('btn1').addEventListener('click', function() {
+  openModal('modal1');
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+  openModal('modal2');
+});
+
+document.getElementById('btn3').addEventListener('click', function() {
+  openModal('modal3');
+});
