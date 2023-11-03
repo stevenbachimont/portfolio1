666a0c7b67ad6fcc9eae78511154d2f6875af0ab
STEVEN BACHIMONT <142581911+stevenbachimont@users.noreply.github.com>
2023-11-03 08:43:19 +0100

    update

--- js/modal.js
+++ js/modal.js
@@ -26,3 +26,7 @@ document.getElementById('btn2').addEventListener('click', function() {
 document.getElementById('btn3').addEventListener('click', function() {
   openModal('modal3');
 });
+
+document.getElementById('btn4').addEventListener('click', function() {
+  openModal('modal4');
+});
