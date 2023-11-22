c5f06e8acc558932bf70d84fd1ad89fea26551ef
STEVEN BACHIMONT <142581911+stevenbachimont@users.noreply.github.com>
2023-11-22 17:47:06 +0100

    update

--- js/fringue.js
+++ js/fringue.js
@@ -1,186 +1,144 @@
+// Paramètres de la simulation
+let timestep        = 0.005;    // Intervalle de temps pour chaque étape de simulation
+let steps_per_frame = 20;       // Nombre d'étapes de simulation par trame d'animation
+let node_mass       = 0.5;      // Masse de chaque nœud du système
+let gravity         = 10;       // Force de gravité
+let spring          = 2000;     // Constante de raideur du ressort
+let damping         = 0.2;      // Facteur d'amortissement pour résister au mouvement
+let max_strain      = 2;        // Déformation maximale autorisée dans un ressort
 
-let timestep        = .005;
-let steps_per_frame = 20;
-let node_mass       = .5;
-let gravity         = 10;
-let spring          = 5000;
-let damping         = .2;
-let max_strain      =  2;
-
-/* - - - Make Arrays for Positions, Velocities, and Connections - - - */
-
+// Initialisation des positions, vitesses et connexions entre les nœuds
 let pos = [], vel = [], mesh = [];
 
-// Make grid of node positions with zero velocity
+// Création des nœuds en grille avec positions et vitesses initiales
 for (let x = 2; x < 800; x += 8) {
-    for (let y = 10; y < 400; y+=8) {
-        pos.push([x,y]);
-        vel.push([0,0]);
+    for (let y = 10; y < 400; y += 8) {
+        pos.push([x, y]);
+        vel.push([0, 0]);
     }
 }
 
-// Loop thru each node 'a'
+// Création des ressorts entre les nœuds en fonction de leur distance
 for (let a = 0; a < pos.length; a++) {
-
-    // Make empty array to store node 'a's connections
     mesh.push([]);
 
-    // Loop thru other nodes 'b'
     for (let b = 0; b < pos.length; b++) {
-
-        // Get distance between 'a' and 'b'
         let Lx = pos[b][0] - pos[a][0];
         let Ly = pos[b][1] - pos[a][1];
-        let L  = Math.sqrt(Lx*Lx + Ly*Ly);
+        let L  = Math.sqrt(Lx * Lx + Ly * Ly);
 
-        // connect 'b' to 'a' if nearby
-        if (L > 0 && L < 15) mesh[a].push( [b, L] );
+        // Ajout du ressort à la connexion s'il est dans une certaine distance
+        if (L > 0 && L < 15) mesh[a].push([b, L]);
     }
 }
 
-// Fix some nodes at the top
+// Suppression aléatoire de certains ressorts en fonction de la position des nœuds
 for (let n = 0; n < mesh.length; n++) {
-    if (pos[n][1]<100 && Math.random()< .02) mesh[n] = [];
+    if (pos[n][1] < 100 && Math.random() < 0.02) mesh[n] = [];
 }
 
-/* - - - Set up Mouse, Canvas and Drawing Context - - - */
-
+// Variables pour l'interaction de la souris
 let mouse_down = 0, mouse_x = 0, mouse_y = 0;
 
+// Gestion des événements de la souris
 canvas.onmousedown = () => mouse_down = 1;
 canvas.onmouseup   = () => mouse_down = 0;
-
 canvas.onmousemove = (e) => {
     mouse_x = e.offsetX;
     mouse_y = e.offsetY;
 }
 
+// Contexte du canvas
 let ctx = canvas.getContext('2d');
 
-/* - - - Run Animation Loop - - - */
-
-(function loop(){
-
+// Boucle principale de la simulation
+(function loop() {
     if (mouse_down) remove_springs_near_mouse();
 
     for (let i = 0; i < steps_per_frame; i++) update();
 
     draw();
     requestAnimationFrame(loop);
+})();
 
-})()
-
-/* - - - Update Function - - - */
-
-// This does a 'timestep' across all nodes
-
+// Fonction de mise à jour de la simulation
 function update() {
-
-    // Loop thru each node 'a'
     for (let a = 0; a < mesh.length; a++) {
-
-        // Loop thru node's connections
         for (let i = 0; i < mesh[a].length; i++) {
-
-            // Get node 'b' and rest length L0 between nodes 'a' and 'b'
             let b  = mesh[a][i][0];
             let L0 = mesh[a][i][1];
-
-            // Get actual Distance between nodes 'a' and 'b'
             let Lx = pos[b][0] - pos[a][0];
             let Ly = pos[b][1] - pos[a][1];
-            let L  = Math.sqrt(Lx*Lx + Ly*Ly);
+            let L  = Math.sqrt(Lx * Lx + Ly * Ly);
+            let strain = (L - L0) / L0;
 
-            // Get strain between nodes 'a' and 'b'
-            let strain = (L - L0)/L0;
-
-            // Break connection if too much strain
+            // Suppression du ressort si la déformation est trop importante
             if (strain > max_strain) {
                 remove_spring(a, b);
-            }
-            // Otherwise, Update Velocity
-            else {
-
-                // Get node 'a' velocity
+            } else {
                 let Vx = vel[a][0];
                 let Vy = vel[a][1];
+                let springF  = Math.max(0, spring * strain);
+                let dampingF = -damping * (Lx * Vx + Ly * Vy) / L;
 
-                // Get Spring and Damping Forces
-                let springF  = Math.max(0, spring*strain);
-                let dampingF = -damping*(Lx*Vx+Ly*Vy)/L;
+                let Fx = (Lx / L) * (springF + dampingF);
+                let Fy = (Ly / L) * (springF + dampingF);
 
-                // Find Force
-                let Fx = (Lx/L)*(springF + dampingF);
-                let Fy = (Ly/L)*(springF + dampingF);
-
-                // Update Velocity
-                vel[a][0] += (Fx/node_mass)*timestep;
-                vel[a][1] += (Fy/node_mass + gravity)*timestep;
+                vel[a][0] += (Fx / node_mass) * timestep;
+                vel[a][1] += (Fy / node_mass + gravity) * timestep;
             }
         }
     }
 
-    // Update Node Position
+    // Mise à jour des positions des nœuds
     for (let n = 0; n < mesh.length; n++) {
-
-        pos[n][0] += vel[n][0]*timestep;
-        pos[n][1] += vel[n][1]*timestep;
+        pos[n][0] += vel[n][0] * timestep;
+        pos[n][1] += vel[n][1] * timestep;
     }
 }
 
-/* - - - Draw Function - - - */
-
+// Fonction de rendu graphique
 function draw() {
-
-    // Clear canvas, begin making a line drawing
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.strokeStyle = "rgba(93,93,93,0.98)";
 
-    // Loop thru each node's connections
+    // Dessin des ressorts entre les nœuds
     for (let a = 0; a < pos.length; a++) {
         for (let i = 0; i < mesh[a].length; i++) {
-
-            // Add line from node 'a' to node 'b'
             let b = mesh[a][i][0];
-
             ctx.moveTo(pos[a][0], pos[a][1]);
             ctx.lineTo(pos[b][0], pos[b][1]);
         }
     }
 
-    // Draw the lines and also a square to show mouse position
+    // Dessin d'un petit rectangle à la position actuelle de la souris
     ctx.stroke();
-    ctx.fillRect(mouse_x-10, mouse_y-10,0,0);
+    ctx.fillRect(mouse_x - 10, mouse_y - 10, 0, 0);
 }
 
-/* - - - Mouse / Interactive Functions - - - */
-
+// Fonction pour supprimer un ressort entre deux nœuds
 function remove_spring(a, b) {
-
-    for (let [A,B] of [[a,b],[b,a]]){
+    for (let [A, B] of [[a, b], [b, a]]) {
         for (let i = 0; i < mesh[A].length; i++) {
-
-            if (mesh[A][i][0] == B) mesh[A].splice(i,1);
+            if (mesh[A][i][0] == B) mesh[A].splice(i, 1);
         }
     }
 }
 
+// Fonction pour supprimer les ressorts proches de la position de la souris
 function remove_springs_near_mouse() {
-
-    let N  = [];
+    let N = [];
 
     for (let n = 0; n < mesh.length; n++) {
-        if (Math.abs(mouse_x - pos[n][0]) < 10) {
-            if (Math.abs(mouse_y - pos[n][1]) < 10) N.push(n);
+        if (Math.abs(mouse_x - pos[n][0]) < 10 && Math.abs(mouse_y - pos[n][1]) < 10) {
+            N.push(n);
         }
     }
 
     for (let a of N) {
         for (let b of mesh[a]) {
-            if (a != b) remove_spring(a,b[0]);
+            if (a != b) remove_spring(a, b[0]);
         }
     }
 }
-
-
