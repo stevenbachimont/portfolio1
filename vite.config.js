4032aabfa3af364bb27fcd05ddefae760633d146
stevenbachimont <stevenbachimont@gmail.com>
2024-05-22 20:49:42 +0200

    deploiment test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/siteweb/",
   css: {
     modules: {
       localsConvention: "camelCase",
     },
   },
-  base: "/PageTest-Vite-react/"
+
 });
