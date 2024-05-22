92c3c9c59536a1457409d7e8c759c4496df179a3
stevenbachimont <stevenbachimont@gmail.com>
2024-05-22 21:27:11 +0200

    modif repertoire cv

---
+++ public/messages.js
@@ -0,0 +1,14 @@
+
+function hiddenMessage() {
+    console.log(
+        "  _____      ___\n" +
+        " / ____|    |  _ \\\n" +
+        "| (___      | |_) |\n" +
+        " \\___ \\     |  _ <\n" +
+        " ____) |    | |_) |\n" +
+        "|_____/     |____/\n"
+    );
+    console.log( "Plus de détails ici :");
+}
+
+hiddenMessage();
