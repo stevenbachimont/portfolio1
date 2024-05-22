3dbcb92f30821f8900fe54ee28d72ae23411dd96
stevenbachimont <stevenbachimont@gmail.com>
2024-05-22 19:48:30 +0200

    gros test deployement nouveau site

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:contact.bachimont@gmail.com" target="_blank">contact.bachimont@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/steven-bachimont-437944276/" target="_blank">linkedin.com/Steven Bachimont</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/stevenbachimont" target="_blank">github.com/stevebachimont</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
