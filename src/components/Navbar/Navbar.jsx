d114bef4b6a855f023002c36ca1f613c6563e397
stevenbachimont <stevenbachimont@gmail.com>
2024-09-15 16:02:35 +0200

    modif titre

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
   return (
       <nav className={styles.navbar}>
         <a className={styles.title} >
-          Steven Bachimont <br/> -Portfolio-
+          Steven Bachimont <br/> -webdevelopper- <br/> -Portfolio-
         </a>
         <div className={styles.menu}>
           <img
